f48df2ef1d53fc7c5964b95b2bf5799f117bd56c
JSRKNG <167060456+JSRKNG@users.noreply.github.com>
2025-06-12 16:38:51 +0530

    Added manifest.json, pop.html, background.js, pop.js -- Learning how background.js works in the context of chrome extensions

--- background.js
+++ background.js
@@ -1,24 +1,8 @@
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.action === 'takeScreenshot') {
-      takeScreenshot(message.filename);
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: {tabId: tab.id},
+    func: () => {
+      document.body.style.backgroundColor = 'lightblue';
     }
   });
-  
-  chrome.commands.onCommand.addListener((command) => {
-    if (command === 'take_default_screenshot') {
-      const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-      const defaultFilename = `screenshot-${timestamp}.png`;
-      takeScreenshot(defaultFilename);
-    }
-  });
-  
-  function takeScreenshot(filename) {
-    chrome.tabs.captureVisibleTab(null, { format: 'png' }, (dataUrl) => {
-      chrome.downloads.download({
-        url: dataUrl,
-        filename: filename,
-        saveAs: false
-      });
-    });
-  }
-  
+});
